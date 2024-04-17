126b0c7ac7c904301db149e0be23a5ab927eeb29
Programerbird <programerbird@gmail.com>
2024-04-16 22:18:32 -0700

    up2

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import pic1 from '../images/pic1.png'
+import pic2 from '../images/pic2.png'
+import pic3 from '../images/pic3.png'
+import pic10 from '../images/pic10.jpg'
+import '../App.css'
 const Body = () => {
   return (
-    <div></div>
+    <>
+      {/* <div >
+        <img className=' relative mx-auto flex justify-center items-center w-100 h-50 ' alt='foodpic' src={pic10} 
+        
+        />
+      </div>
+      */}
+        
+        <div className='main_body'>
+          <div className='text-black bg-red-500 h-80 text-6xl  text-center flex justify-center items-center  ' >
+            <h1>Welcome To</h1>
+            
+          </div>
+        </div>
+
+    </>
   )
 }
 
--- src/components/Nabar.jsx
+++ src/components/Nabar.jsx
@@ -5,8 +5,8 @@ const Nabar = () => {
     <>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
 <div class="container-fluid">
-<img src={logo} className='h-28 w-21'/>
- <a class="navbar-brand" href="#">Fresh Fusion Catering</a> 
+<img src={logo} className='h-28 w-21  '/>
+ <a class="navbar-brand " href="#">Fresh Fusion Catering</a> 
 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon  " ></span>
 </button>
