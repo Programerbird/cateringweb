0647e705e06af502f2620bb29534d6b3688b490b
Programerbird <programerbird@gmail.com>
2024-05-06 19:42:10 -0700

    realup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import pic1 from '../images/pic1.png'
 import pic2 from '../images/pic2.png'
 import pic3 from '../images/pic3.png'
 import pic10 from '../images/pic10.jpg'
+import chef from '../images/chef.jpg'
 import '../App.css'
 import Contact from './Contact'
  const Home = () => {
@@ -22,14 +23,13 @@ import Contact from './Contact'
         <div>
         </div>
       </div>
-        <div>
-          <h1 className='text-black text-5xl flex justify-center py-3 ' >About us </h1>
+        <div className='ab' >
+          <h1 className='text-black text-5xl flex justify-center py-3 text-white' >About us </h1>
           <h2 className='flex justify-center text-center ' >Catering is not just about food. it's about creating memorable experiences.Good catering is the secret<br/>
            ingredient thta makes an event truly unforgettable.We provide delicious food on time.
            Good eating makes you think positively. </h2>
-           <div className='all_img' >
-          <img  src={pic1} />
-          <img  src={pic3} />
+          <div className='chef' >
+            <img src={chef} />
           </div>
         </div>
         <Contact />
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,6 +9,33 @@ import '../App.css'
             Food is art and science. So, learn, experiment, and don’t be afraid to make mistakes.<br/>
             Catering is not just about feeding people; it's about creating an experience</h2>
         </div>
+          <div className='breakfast' >
+            <h1 className='flex justify-center text-4xl' >Breakfast</h1>
+            <h2 className='text-white py-9 text-2xl' >Continental Breakfast</h2>
+            <p>Bagels, yogurt, bananas, danish, fresh brewed coffee, <br/>
+            orange or apple juice, fresh fruit platter, free setup <br/>
+            $15 per person <spa className="text-white text-xs " >50 person minimum.</spa></p>
+            <h2 className='flex justify-center text-2xl py-5 text-white ' >Hot Breakfast</h2>
+            <p className=' ' >Scrambled eggs, western eggs, breakfast sausage, bacon <br/>
+            french toast, roasted potatoes, freshbagels<br/>
+            orange or apple juice, all condiments free setup<br/>
+            $20 per person <span className='text-white text-xs' >50 person minimum</span></p>
+          </div>
+
+            <div className='lunch' >
+              <h1 className='flex justify-center text-4xl' >Lunch</h1>
+                <h2 className='py-5 text-2xl text-white' >Cold Lunch</h2>
+                  <p>Grill Chicken Sandwich, Bacon Turkey Sandwhich, BLT Sandwhich<br/>
+              Veggie Sandwich, Tuna Melt Sandwhich Steak Wrap, Fish Wrap,<br/>
+              Veggie Wrap, Shrimp Wrap, Pork wrap, Caesar Salad <br/>
+              $25 per person <span className='text-white text-xs' >50 person minimum</span></p>
+              <h2 className='flex justify-center py-9 text-2xl text-white' >Hot Lunch</h2>
+                <p>Wine Pasta with Mushrooms,Spinach Lasagna, Creamy Tomato Rigatoni<br/>
+                Fettuccine Alfredo,Buttermilk Macaroni and Cheese with Baby Kale<br/>
+                Spaghetti and Meatballs, Mac and Cheese, Creamy Tomato Pasta, Shrimp Alfredo, Meat Lasagna<br/>
+                $30 per person <span className='text-white text-xs' >50 person minimum</span> </p>
+            </div>
+         
        
     </>
   )
