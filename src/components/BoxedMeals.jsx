998b2473d1f9cacb183497acc37a31dce6650e1c
Programerbird <programerbird@gmail.com>
2024-05-07 20:17:22 -0700

    up3

--- src/components/BoxedMeals.jsx
+++ src/components/BoxedMeals.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
-
+import '../App.css'
+import Box from '@mui/material/Box';
 const BoxedMeals = () => {
+
   return (
-    <div>BoxedMeals</div>
+    <>
+  
+    <div className='box_m' >
+      <h1 className='flex justify-center text-6xl underline ' >Boxed Meals</h1>
+      <h2 className=' text-white text-lg py-3 ' >"Cooking is all about people. Food is maybe the only universal thing that really has the power<br/> to bring everyone together.
+      No matter what culture, everywhere around the world, people get together to eat.<br/>
+      "Catering is an art. It’s about creating an atmosphere and a vibe to make a lasting impression.<br/>
+      "Great food has a kind of magic. It makes families, friends, and communities come together.</h2>
+      <hr className=' border-3 text-white ' ></hr>
+      <h3 className='text-white py-9 text-3xl ' >BreakFast Box Meals</h3>
+      
+        <div className='sq_1' >
+        <Box
+          height={300}
+          width={400}
+          my={4}
+          display="flex"
+          alignItems="center"
+          gap={4}
+          p={2}
+          sx={{ border: '5px solid grey' }}
+          
+        >
+          <ul className='' >
+            <h1 className='text-2xl text-white underline py-2 ' >Foods</h1>
+            <li>★ Muffins</li>
+            <li>★ Bagels & Cream Cheese</li>
+            <li>★ Croissants</li>
+            <li>★ Biscuits & Jam</li>
+            <li>★ Granola Bars</li>
+            <li>★ Yogurts</li>
+            <li>★ Hard Boiled Eggs</li>
+            <li>★ Whole Fruit</li>
+            <li>★ Pancakes</li>
+          </ul>
+        
+        </Box>
+        </div>
+    </div>
+
+    </>
   )
 }
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,26 +4,26 @@ import '../App.css'
   return (
     <>
         <div className='menu' >
-            <h1 className='flex justify-center py-3  text-6xl ' >Menu</h1>
+            <h1 className='flex justify-center py-3  text-7xl underline ' >Menu</h1>
             <h2 className='flex justify-center text-white text-lg ' >The people who give you their food give you their heart. Life is too short to eat boring food.<br/>
             Food is art and science. So, learn, experiment, and don’t be afraid to make mistakes.<br/>
             Catering is not just about feeding people; it's about creating an experience</h2>
         </div>
           <div className='breakfast' >
-            <h1 className='flex justify-center text-4xl' >Breakfast</h1>
+            <h1 className='flex justify-center text-6xl underline' >Breakfast</h1>
             <h2 className='text-white py-9 text-2xl' >Continental Breakfast</h2>
             <p>Bagels, yogurt, bananas, danish, fresh brewed coffee, <br/>
             orange or apple juice, fresh fruit platter, free setup <br/>
             $15 per person <spa className="text-white text-xs " >50 person minimum.</spa></p>
             <h2 className='flex justify-center text-2xl py-5 text-white ' >Hot Breakfast</h2>
-            <p className=' ' >Scrambled eggs, western eggs, breakfast sausage, bacon <br/>
+            <p className=' ' >Scrambled eggs, western eggs, breakfast sausage, bacon, <br/>
             french toast, roasted potatoes, freshbagels<br/>
             orange or apple juice, all condiments free setup<br/>
             $20 per person <span className='text-white text-xs' >50 person minimum</span></p>
           </div>
 
             <div className='lunch' >
-              <h1 className='flex justify-center text-4xl' >Lunch</h1>
+              <h1 className='flex justify-center text-6xl underline' >Lunch</h1>
                 <h2 className='py-5 text-2xl text-white' >Cold Lunch</h2>
                   <p>Grill Chicken Sandwich, Bacon Turkey Sandwhich, BLT Sandwhich<br/>
               Veggie Sandwich, Tuna Melt Sandwhich Steak Wrap, Fish Wrap,<br/>
@@ -35,6 +35,20 @@ import '../App.css'
                 Spaghetti and Meatballs, Mac and Cheese, Creamy Tomato Pasta, Shrimp Alfredo, Meat Lasagna<br/>
                 $30 per person <span className='text-white text-xs' >50 person minimum</span> </p>
             </div>
+              <div className='dinner' >
+                <h1 className='flex justify-center text-6xl underline' >Dinner</h1>
+                  <h2 className='py-5 text-2xl text-white' >Main Dinner</h2>
+                    <p>Spagetti, Fettucini Alfredo, Vegan Pesto Pasta, Traditional<br/>
+                    Roasted Vegetable Lasagna, Lemon-Herb Chicken, BBQ-Chicken, <br/>
+                    Grilled Hamburgers, Grilled Hot Dog, Roasted Vegetable Slider<br/>
+                    $40 per person <span className='text-white text-xs' >100 person minimum</span></p>
+                      <h3 className='py-5 text-2xl text-white' >Chef's Choice Dinner</h3>
+                        <p>Pulled Pork Sliders, tri-tip Slider, BBQ Ribs, Boursin Stuffed Chicken,<br/>
+                        Stuffed Pork Loin, Meat Balls, Roasted Turkey, Roasted Vegetable Skewers,<br/>
+                        Pork Tenderloin, New York Strip, Seared Salmon<br/>
+                        $50 per person <span className='text-white text-xs ' >100 person minimum</span></p>
+
+              </div>
          
        
     </>
